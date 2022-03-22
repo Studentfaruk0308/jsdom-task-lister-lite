3febdf7ec49824098b6e9b2c6bb59c3d7364b05d
Studentfaruk0308 <studentfaruk0308@gmail.com>
2022-03-22 22:52:56 +1100

    Andre Format

--- src/index.js
+++ src/index.js
@@ -1,15 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-  
-  const taskForm = document.querySelector('#create-task-form');
-  const taskList = dcoument.querySelector('#tasks');
+  const newTaskForm = document.getElementById("create-task-form");
+  const newTaskDescription = document.getElementById("new-task-description");
+ 
+  const taskUl = document.getElementById("tasks");
+ 
+  newTaskForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const taskLi = document.createElement('li');
+    //elements inside list
+    const deleteButton = document.createElement('button');
+    const updateButton = document.createElement('button');
+    const spanText = document.createElement('span');
+ 
+    spanText.innerHTML = newTaskDescription.value;
+    taskUl.append(taskLi);
+    deleteButton.innerHTML = 'X';
+    updateButton.innerHTML = 'Update';
+ 
+    taskLi.append(spanText);
+    taskLi.append(deleteButton);
+    taskLi.append(updateButton);
+   
+    updateButton.addEventListener('click', (event) => {
+      spanText.innerHTML = newTaskDescription.value;
+      e.target.reset();
+    })
 
-taskForm.addEventListener('submit', function(e) {
-  e.preventDefault();
-  const newTask = document.querySelector('#new-task-description')
-  const taskItem = document.createElement('li')
-  taskItem.innerText = newTask.value
-  taskList.appendChild(taskItem)
-})
-
-});
+    deleteButton.addEventListener('click', (event) => {
+      taskLi.remove();
+    })
+    e.target.reset();
+  });
+ });
