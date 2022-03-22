a45e2cb2a66b09096e655b0b0238bb0036450c23
faruk <studentfaruk0308@gmail.com>
2022-03-22 20:08:57 +1100

    Part

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  
+  const taskForm = document.querySelector('#create-task-form');
+  const taskList = dcoument.querySelector('#tasks');
+
+taskForm.addEventListener('submit', function(e) {
+  e.preventDefault();
+  const newTask = document.querySelector('#new-task-description')
+  const taskItem = document.createElement('li')
+  taskItem.innerText = newTask.value
+  taskList.appendChild(taskItem)
+})
+
 });
